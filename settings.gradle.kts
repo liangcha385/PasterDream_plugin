98bb85813767e88d28d44980f52ea2dce16358e6
凉茶是帅哥 <108937242+liangcha385@users.noreply.github.com>
2024-01-19 10:03:44 +0800

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name="PasterDream_plugin"
---
+++ src/main/kotlin/io/github/liangcha385/PasterDream_plugin.kt
@@ -0,0 +1,11 @@
+package io.github.liangcha385
+
+import taboolib.common.platform.Plugin
+import taboolib.common.platform.function.info
+
+object PasterDream_plugin : Plugin() {
+
+    override fun onEnable() {
+        info("Successfully running ExamplePlugin!")
+    }
+}
